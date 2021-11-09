216d14f58aa31d20a7b01f9995a0598ecd016c0b
Mike Belot <mike.belot@googlecloud.corp-partner.google.com>
2021-11-09 10:59:16 -0800

    refactoring

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -1,28 +1,28 @@
-//data "google_bigquery_default_service_account" "bq_sa" {
-//}
-//
-//module "bigquery_crypto" {
-//  source            = "./modules/gcp-kms/crypto-key"
-//
-//  keyring_name      = "tfvalidator-demo-keyring"
-//  key_name          = "bq_crypto_key"
-//  kms_binding_members = ["serviceAccount:${data.google_bigquery_default_service_account.bq_sa.email}"]
-//}
+data "google_bigquery_default_service_account" "bq_sa" {
+}
 
-//resource "google_bigquery_dataset" "dataset" {
-//  provider = var.project_id
-//  dataset_id                  = "demo_dataset"
-//  friendly_name               = "test"
-//  description                 = "This is a demo dataset"
-//  location                    = "us-east4"
-//  default_table_expiration_ms = 3600000
-//
-//  default_encryption_configuration {
-//    kms_key_name = module.bigquery_crypto.crypto_key_id
-//  }
-//
-//  # Ensure the KMS crypto-key IAM binding for the service account exists prior to the
-//  # bq table attempting to utilise the crypto-key.
-//  depends_on = [module.bigquery_crypto]
-//}
+module "bigquery_crypto" {
+  source            = "./modules/gcp-kms/crypto-key"
+
+  keyring_name      = module.project_keyring.keyring_id
+  key_name          = "bq_crypto_key"
+  kms_binding_members = ["serviceAccount:${data.google_bigquery_default_service_account.bq_sa.email}"]
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  provider = var.project_id
+  dataset_id                  = "demo_dataset"
+  friendly_name               = "test"
+  description                 = "This is a demo dataset"
+  location                    = "us-east4"
+  default_table_expiration_ms = 3600000
+
+  default_encryption_configuration {
+    kms_key_name = module.bigquery_crypto.crypto_key_id
+  }
+
+  # Ensure the KMS crypto-key IAM binding for the service account exists prior to the
+  # bq table attempting to utilise the crypto-key.
+  depends_on = [module.bigquery_crypto]
+}
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,19 +6,19 @@ module "project_keyring" {
   keyring_location = "us"
 }
 
-resource "google_kms_crypto_key" "crypto_key" {
-  name     = "test-key-1"
-  key_ring = "tfvalidator-demo-keyring"
-  purpose  = "ENCRYPT_DECRYPT"
-
-  # 90 day rotation period
-  rotation_period = "7776000s"
-
-  version_template {
-    algorithm        = "GOOGLE_SYMMETRIC_ENCRYPTION"
-    protection_level = "SOFTWARE"
-  }
-  //  lifecycle {
-  //    prevent_destroy = true
-  //  }
-}
+//resource "google_kms_crypto_key" "crypto_key" {
+//  name     = "tfv-test-key-2"
+//  key_ring = module.project_keyring.keyring_id
+//  purpose  = "ENCRYPT_DECRYPT"
+//
+//  # 90 day rotation period
+//  rotation_period = "7776000s"
+//
+//  version_template {
+//    algorithm        = "GOOGLE_SYMMETRIC_ENCRYPTION"
+//    protection_level = "SOFTWARE"
+//  }
+//  //  lifecycle {
+//  //    prevent_destroy = true
+//  //  }
+//}
