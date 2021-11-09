54efef1f9d339e34b016104e968ac497a9e53b5f
Mike Belot <mike.belot@googlecloud.corp-partner.google.com>
2021-11-08 18:14:57 -0800

    disabling resource creation

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -5,23 +5,24 @@ module "bigquery_crypto" {
   source            = "./modules/gcp-kms/crypto-key"
 
   keyring_name      = "tfvalidator-demo-keyring"
-  key_name   = "bq_crypto_key"
+  key_name          = "bq_crypto_key"
   kms_binding_members = ["serviceAccount:${data.google_bigquery_default_service_account.bq_sa.email}"]
 }
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id                  = "demo_dataset"
-  friendly_name               = "test"
-  description                 = "This is a demo dataset"
-  location                    = "us-east4"
-  default_table_expiration_ms = 3600000
-
-  default_encryption_configuration {
-    kms_key_name = module.bigquery_crypto.crypto_key_id
-  }
-
-  # Ensure the KMS crypto-key IAM binding for the service account exists prior to the
-  # bq table attempting to utilise the crypto-key.
-  depends_on = [module.bigquery_crypto]
-}
+//resource "google_bigquery_dataset" "dataset" {
+//  provider = var.project_id
+//  dataset_id                  = "demo_dataset"
+//  friendly_name               = "test"
+//  description                 = "This is a demo dataset"
+//  location                    = "us-east4"
+//  default_table_expiration_ms = 3600000
+//
+//  default_encryption_configuration {
+//    kms_key_name = module.bigquery_crypto.crypto_key_id
+//  }
+//
+//  # Ensure the KMS crypto-key IAM binding for the service account exists prior to the
+//  # bq table attempting to utilise the crypto-key.
+//  depends_on = [module.bigquery_crypto]
+//}
 
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -5,19 +5,20 @@ module "gcs_crypto" {
   source            = "./modules/gcp-kms/crypto-key"
 
   keyring_name      = "tfvalidator-demo-keyring"
-  key_name   = "gcs_crypto_key"
+  key_name          = "gcs_crypto_key"
   kms_binding_members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
 }
 
-resource "google_storage_bucket" "bucket" {
-  name = "kms-protected-bucket"
-  location = "us-east1"
-
-  encryption {
-    default_kms_key_name = module.gcs_crypto.crypto_key_id
-  }
-
-  # Ensure the KMS crypto-key IAM binding for the service account exists prior to the
-  # bucket attempting to utilise the crypto-key.
-  depends_on = [module.gcs_crypto]
-}
+//resource "google_storage_bucket" "bucket" {
+//  project = var.project_id
+//  name = "kms-protected-bucket"
+//  location = "us-east4"
+//
+//  encryption {
+//    default_kms_key_name = module.gcs_crypto.crypto_key_id
+//  }
+//
+//  # Ensure the KMS crypto-key IAM binding for the service account exists prior to the
+//  # bucket attempting to utilise the crypto-key.
+//  depends_on = [module.gcs_crypto]
+//}
