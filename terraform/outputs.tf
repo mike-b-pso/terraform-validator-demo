06a9dbced8fbd6352811f4c5c74796189bf35073
Mike Belot <mike.belot@googlecloud.corp-partner.google.com>
2021-11-08 18:17:05 -0800

    commenting out resources outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,10 +11,10 @@ output "bq_crypto_key_id" {
   value = module.bigquery_crypto.crypto_key_id
 }
 
-output "gcs_bucket_name" {
-  value = google_storage_bucket.bucket.self_link
-}
-
-output "gcs_bucket_url" {
-  value = google_storage_bucket.bucket.url
-}
+//output "gcs_bucket_name" {
+//  value = google_storage_bucket.bucket.self_link
+//}
+//
+//output "gcs_bucket_url" {
+//  value = google_storage_bucket.bucket.url
+//}
