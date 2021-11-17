414dd77b0d2106f4315a9c184a0b59181cfc14db
Mike Belot <mike.belot@googlecloud.corp-partner.google.com>
2021-11-17 12:50:05 -0800

    testing

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,18 +3,18 @@ output "project_keyring_id" {
   value = module.project_keyring.keyring_id
 }
 
-output "gcs_crypto_key_id" {
-  value = module.gcs_crypto.crypto_key_id
-}
+//output "gcs_crypto_key_id" {
+//  value = module.gcs_crypto.crypto_key_id
+//}
 
 output "bq_crypto_key_id" {
   value = module.bigquery_crypto.crypto_key_id
 }
 
-output "gcs_bucket_name" {
-  value = google_storage_bucket.bucket.self_link
-}
-
-output "gcs_bucket_url" {
-  value = google_storage_bucket.bucket.url
-}
+//output "gcs_bucket_name" {
+//  value = google_storage_bucket.bucket.self_link
+//}
+//
+//output "gcs_bucket_url" {
+//  value = google_storage_bucket.bucket.url
+//}
